a49b0ebb3e26eff8f6a1f69ef5a577a6716ae7f9
Paul Asyn <paulasyn@gmail.com>
2024-08-13 16:11:35 -0700

    added more dummy data

--- src/app/lib/placeholder-data.tsx
+++ src/app/lib/placeholder-data.tsx
@@ -13,9 +13,42 @@ export const cards = [
         card_id: "001",
         card_idol: "Jennie",
         card_group: "blackpink",
+		card_album: "test_album",
         card_original_user_id: "001",
         group_photo: false,
         date_created: "2022-01-01",
+		card_photo_loc: "/cardPhotos/BLACKPINK-4-1-POP-UP-Photocards-SCANS-documents-1 (1).jpeg",
+    },
+	{
+        card_id: "002",
+        card_idol: "Rose",
+        card_group: "blackpink",
+		card_album: "test_album",
+        card_original_user_id: "001",
+        group_photo: false,
+        date_created: "2022-01-01",
+		card_photo_loc: "/cardPhotos/BLACKPINK-4-1-POP-UP-Photocards-SCANS-documents-1 (1).jpeg",
+    },
+	{
+        card_id: "003",
+        card_idol: "Lisa",
+        card_group: "blackpink",
+		card_album: "test_album",
+        card_original_user_id: "001",
+        group_photo: false,
+        date_created: "2022-01-01",
+		card_photo_loc: "/cardPhotos/BLACKPINK-4-1-POP-UP-Photocards-SCANS-documents-1 (1).jpeg",
+    },
+	{
+        card_id: "004",
+        card_idol: "Jisoo",
+        card_group: "blackpink",
+		card_album: "test_album",
+        card_original_user_id: "001",
+        group_photo: false,
+        date_created: "2022-01-01",
+		card_photo_loc: "/cardPhotos/BLACKPINK-4-1-POP-UP-Photocards-SCANS-documents-1 (1).jpeg",
+
     }
 ]
 
@@ -29,11 +62,33 @@ export const idols = [
     }
 ]
 
+export const decks = [
+	{
+		deck_id: "001",
+		deck_cards: cards,
+		deck_name: "test_deck",
+		deck_group_assoc: "blackpink",
+		deck_user_id: "001",
+		date_created: "2022-01-01",
+		deck_photo_loc: "/cardPhotos/BLACKPINK-4-1-POP-UP-Photocards-SCANS-documents-1 (1).jpeg",
+	},
+	{
+		deck_id: "002",
+		deck_cards: cards,
+		deck_name: "test_deck",
+		deck_group_assoc: "blackpink",
+		deck_user_id: "001",
+		date_created: "2022-01-01",
+		deck_photo_loc: "/cardPhotos/BLACKPINK-4-1-POP-UP-Photocards-SCANS-documents-2 (1).jpeg",
+	}
+]
+
 export const groups =  [
     {
         group_id: "001",
         group_name: "test_group",
         group_members: ["Lisa", "Rose", "Jennie", "Jisoo"],
+		assoc_decks: ["001"],
         date_created: "2022-01-01",
     }
 ]
